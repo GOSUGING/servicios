20e1bfcedff197d40c35b34d4ee4a3bf2bdba4e7
Aron <aroon.suy@gmail.com>
2024-04-18 00:48:24 -0400

    se agregaron nuevas lineas al index.js, se agregaron nuevas lineas en archivos handlebars y se agrego archivos para vista pública

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const app = express();
 app.use(express.static('public'))
 
 app.use('/css', express.static('node_modules/bootstrap/dist/css'))
-
+app.use('/js', express.static('node_modules/bootstrap/dist/js'))
+app.use('/js', express.static('node_modules/jquery/dist'))
 // Handlebars
 app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
