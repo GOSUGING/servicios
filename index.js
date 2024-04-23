6abea78e5a7b0b8e3403ad22926a0280ec8a0a7a
Aron <aroon.suy@gmail.com>
2024-04-23 14:12:54 -0400

    se agregaron nuevas lineas al index.js, se agrego un service.hbs que redirige a las páginas

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import express from 'express';
 import  { engine } from 'express-handlebars';
+import { services } from './data/services.data.js';
+import path from 'path'
+
 
 const app = express();
+
+const __dirname = import.meta.dirname;
+console.log(__dirname)
+console.log(path.join(__dirname, '/views/layaouts/main.hbs'))
 
 // Public directory
 app.use(express.static('public'))
@@ -9,18 +16,30 @@ app.use(express.static('public'))
 app.use('/css', express.static('node_modules/bootstrap/dist/css'))
 app.use('/js', express.static('node_modules/bootstrap/dist/js'))
 app.use('/js', express.static('node_modules/jquery/dist'))
+
 // Handlebars
+
 app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', './views');
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', {title: 'Home Page 2.0'});
 });
 
 app.get('/services', (req, res) =>{
-    res.render('services');
+    res.render('services', {services: services});
 });
+
+app.get('/services/:name', (req, res) => {
+    
+    const nameUrl = req.params.name
+
+    const service = services.find((item) => item.url === `/services/${nameUrl}`)
+    console.log(service)
+
+   return res.render('service', { service })
+}); 
 
 app.listen(5001, () => {
     console.log(`Servidor encendido http://localhost:${5001}`);
